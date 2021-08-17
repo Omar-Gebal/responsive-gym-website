6f850efbecc7846283e7d2ce3fb4aa88e0e6872d
Omar Emad <2001omaremad@gmail.com>
2021-08-17 13:54:59 +0200

    Fixed nav bar on mobile bug

--- multi use scripts/navbar.js
+++ multi use scripts/navbar.js
@@ -1,17 +1,20 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.querySelector("nav");
+if(window.innerWidth > 709){
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+  // Get the navbar
+  var navbar = document.querySelector("nav");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {  
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
+}
 }
